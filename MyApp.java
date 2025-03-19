ef210b7af4d348a476391a24d7dbc97da743e2ef
palle4g <gayatri_palle@yahoo.com>
2025-03-19 09:29:17 -0400

    Update MyApp.java
    Changed the name of class to BasicUtil

--- MyApp.java
+++ MyApp.java
@@ -5,7 +5,7 @@ public class MyApp
         // adding dummy comment
         public static void main(String args[]) throws Exception {
             System.out.println("MyApp");                       
-            CryptoUtil cryptoUtilObj = new CryptoUtil();
+            BasicUtil cryptoUtilObj = new BasicUtil();
             
             System.out.println("Encrypted SSN returned for 136805132 is--" +cryptoUtilObj.encrypt("136805132"));
             System.out.println("Decrypted SSN returned for GpjiLWrz/jaBgp+kb0yqgA== is--" +cryptoUtilObj.decrypt("GpjiLWrz/jaBgp+kb0yqgA=="));
